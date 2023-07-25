9eb2ff8a0e684574e34d1b1626b2b1e82830b5d6
Vítor Barroso <contact@owozsh.dev>
2023-07-24 22:44:12 -0300

    dockerize application

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :twdo_api, TwdoApi.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "twdo_api_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
